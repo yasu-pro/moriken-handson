039b4c8f3ae04b38d6ba6f9cf221f14c27414639
yasu-pro <cb1-y.s@docomo.ne.jp>
2021-02-27 21:13:41 +0900

    task_6 done

---
+++ task/task-6/index.js
@@ -0,0 +1,40 @@
+const createLi = document.createElement("li");
+const ulTag = document.getElementsByTagName("ul")[0];
+
+const obj = [
+  { to: "bookmark.html", img: "1.png", alt: "画像1", text: "ブックマーク" },
+  { to: "message.html", img: "2.png", alt: "画像2", text: "メッセージ" },
+];
+
+function task_1() {
+  ulTag.appendChild(createLi);
+}
+
+const result = new Promise((resolve) => {
+  //task-6
+  setTimeout(() => {
+    resolve(obj);
+  }, 3000);
+});
+
+function task_5() {
+  task_1();
+
+  result.then((data) => {
+    data.forEach(function (item, index) {
+      const createAnchor = document.createElement("a");
+
+      createAnchor.setAttribute("href", item["to"]);
+      const createImg = document.createElement("img");
+      createImg.setAttribute("src", item["img"]);
+      createImg.setAttribute("alt", item["alt"]);
+      createAnchor.textContent = item["text"];
+
+      ulTag.appendChild(createLi);
+      createAnchor.prepend(createImg);
+      createLi.appendChild(createAnchor);
+    });
+  });
+}
+
+task_5();
